0a04f61c7e9306dd73caae8af8dbdcc6e0bc2382
NobodysC <nelsonquispecarpio@gmail.com>
2026-07-19 19:53:15 -0500

    cambio de puerto

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
